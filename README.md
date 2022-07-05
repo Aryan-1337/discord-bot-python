e16f53a7af46559080af45af9f3cf41eefb0b144
Aryan <99039079+Aryan-1337@users.noreply.github.com>
2022-07-05 17:58:43 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# discord-bot-python
-maybe later.
+# Discord Bot Python
+
+* A Discord Bot Using [*NextCord*](https://docs.nextcord.dev/en/stable) In [*Python*](https://python.org)
+* 10 :star: Stars And I Will Make Slash Commands Also :P
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# ok
+# IMPORTS
+import nextcord
+from nextcord.ext import commands
+import os
+
+# TOKEN AND PREFIX
+TOKEN = os.getenv('TOKEN')
+PREFIX = "."
+
+# BOT
+intents = nextcord.Intents.all()
+client = discord.Client()
+client = commands.Bot(command_prefix = PREFIX, case_insensitive = True, help_command = None, intents = intents)
+
+# BOT EVENT
+@client.event
+async def on_ready():
+    print(f"Logged In As {client.user}")
+
+# PING COMMAND
+@client.command(aliases=['latency'])
+async def ping(ctx):
+    await ctx.reply(f"**My Ping Is: `{round(client.latency * 1000)}` Ms.**")
+
+client.run(TOKEN)
